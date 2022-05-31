4bac4634d56a2358c3809ff8be8302a055a61a90
levaakai <84318826+levaakai@users.noreply.github.com>
2022-05-31 00:57:29 +0000

    Name Change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react' 
+import Header from './components/Header' 
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+import Footer from './components/Footer'
+import About from './components/About'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [tasks, setTasks] = useState([])
+
+const [showFormPane, setShowFormPane] = useState(false) 
+
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    })
+
+    const data = await res.json()
+
+    setTasks(
+      tasks.map(task => task.id === id ? {...task, reminder: data.reminder } : task)
+  ) }
+
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      'method': 'DELETE'
+    })
+    setTasks(tasks.filter(task => task.id !== id))
+  }
+
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: "POST",
+      headers: {
+        'Content-type': 'application/json'   
+      },
+      body: JSON.stringify(task)
+    })
+    const data = await res.json()
+    setTasks([...tasks, data])
+    // const id = Math.floor(Math.random() * 10000) + 1 
+    // const newTask = {id, ...task} 
+    // setTasks([...tasks, newTask]) 
+  }
+
+
+  // useEffect(()=>{
+  //   const g = JSON.parse(localStorage.getItem('tasks'))
+  //   if (g !==null) setTasks(g);
+
+  // }, [])
+
+  // useEffect(()=>{
+  //   localStorage.setItem("tasks", JSON.stringify(tasks));
+  // }, [tasks])
+
+
+
+  useEffect(() => {
+  const getTasks = async () =>{
+    const fetchedTasks = await fetchTasks()
+    setTasks(fetchedTasks)
+  }
+  getTasks()
+}, [])
+
+
+// function for retrieve tasks from database api
+const fetchTasks = async () => {
+  const res = await fetch('http://localhost:5000/tasks')
+  const data = await res.json()
+  return data
+}
+const fetchTask = async (id) => {
+  const res = await fetch(`http://localhost:5000/tasks/${id}`)
+  const data = await res.json()
+  return data
 }
 
-export default App;
+return (
+      <Router>
+        <div className="container">
+          <Header onAdd={()=>{setShowFormPane(!showFormPane)}} showForm={showFormPane}/>
+          {/* {showFormPane && <AddTask onAdd={addTask} />}
+          {tasks.length > 0 ?
+            <Tasks 
+              tasks={tasks} 
+              onDelete={deleteTask} 
+              onToggle={toggleReminder} /> : <p><i>List is empty</i></p>
+          } */}
+            <Routes>
+              <Route path="/" element={
+                <>
+                  {showFormPane && <AddTask onAdd={addTask} />}
+                  {tasks.length > 0 ?
+                    <Tasks 
+                      tasks={tasks} 
+                      onDelete={deleteTask} 
+                      onToggle={toggleReminder} /> : <p><i>List is empty</i></p>
+                  }
+                </>
+              } />
+              <Route path="/about" element={ <About /> } />
+            </Routes>
+          <Footer />
+        </div>
+      </Router>
+  ) 
+}
+
+export default App 
